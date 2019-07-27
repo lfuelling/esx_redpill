56596040007483317adfad7077a08e9adda5c48e
Lukas Fülling <lukas@k40s.net>
2019-07-27 19:15:44 +0200

    [#6] add logging

--- html/ui.js
+++ html/ui.js
@@ -1,5 +1,5 @@
 $(function () {
-    let term;
+    var term;
     window.addEventListener('message', function (event) {
         if (event.data.type === "enableui") {
             document.body.style.display = event.data.enable ? "block" : "none";
@@ -13,11 +13,15 @@ $(function () {
                     greetings: 'Welcome to Redpill OS v0.1',
                     prompt: event.data.machine.user + '@' + event.data.machine.hostname + ' $ '
                 });
+                console.log('Terminal initialized.')
             } else {
                 term = undefined;
+                console.log('Terminal removed.')
             }
         } else if(event.data.type === "terminalOut") {
-            if(term) {
+            console.log('terminalOut received!');
+            if(term !== undefined) {
+                console.log('printing: \'' + event.data.output + '\'');
                 term.echo(event.data.output);
             }
         }
