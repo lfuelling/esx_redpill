e326bdc8f38b994f5c8160c24c0f5e2c490fb961
Lukas Fülling <lukas@k40s.net>
2019-07-27 14:57:00 +0200

    fix terminal initialization

--- html/ui.js
+++ html/ui.js
@@ -2,6 +2,22 @@ $(function () {
     window.addEventListener('message', function (event) {
         if (event.data.type === "enableui") {
             document.body.style.display = event.data.enable ? "block" : "none";
+
+            if(event.data.enable) {
+                $('#screen').terminal(function (command, term) {
+                    term.pause();
+                    $.post('http://esx_redpill/command', {command: command}).then(function (response) {
+                        if(response.result) {
+                            term.echo(response.print).resume();
+                        } else {
+                            term.echo("Error: " + response.print).resume();
+                        }
+                    });
+                }, {
+                    greetings: 'Welcome to Redpill OS v0.1',
+                    prompt: '$ '
+                });
+            }
         }
     });
 
@@ -10,19 +26,5 @@ $(function () {
         if (e.keyCode === 27) {
             $.post('http://esx_redpill/escape', JSON.stringify({}));
         }
-    });
-
-    $('#screen').terminal(function (command, term) {
-        term.pause();
-        $.post('http://esx_redpill/command', {command: command}).then(function (response) {
-            if(response.result) {
-                term.echo(response.print).resume();
-            } else {
-                term.echo("Error: " + response.print).resume();
-            }
-        });
-    }, {
-        greetings: 'Welcome to Redpill OS v0.1',
-        prompt: '$ '
     });
 });
