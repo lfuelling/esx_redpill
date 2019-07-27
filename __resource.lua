325a411305b546bcff792c3cb5fffa72788bbf81
Lukas Fülling <lukas@k40s.net>
2019-07-27 22:25:03 +0200

    version bump

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Hacking Mission/Job'
 
-version '0.0.1'
+version '0.0.2'
 
 dependency 'bob74_ipl'
 dependency 'es_extended'
