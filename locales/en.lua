a16b748df33910a313550e57aac8850d751ffc77
Lukas Fülling <lukas@k40s.net>
2020-05-07 01:04:57 +0200

    cleanup locales.lua

--- locales/en.lua
+++ locales/en.lua
@@ -3,6 +3,7 @@ Locales['en'] = {
     ['press_interact_to_hack'] = "Press ~INPUT_CONTEXT~ to start ~b~hacking~s~.",
     ['press_interact_to_exit'] = "Press ~INPUT_CONTEXT~ to exit.",
     ['press_interact_to_quit'] = "Press ~INPUT_CONTEXT~ to exit and stop ~b~hacking~s~.",
+
     ['dow_0'] = "Sun",
     ['dow_1'] = "Mon",
     ['dow_2'] = "Tue",
@@ -10,6 +11,7 @@ Locales['en'] = {
     ['dow_4'] = "Thu",
     ['dow_5'] = "Fri",
     ['dow_6'] = "Sat",
+
     ['intro_msg_subtitle1'] = "Hacker?! (1/2)",
     ['intro_msg_subtitle2'] = "Hacker?! (2/2)",
     ['intro_msg_subtitle_quit'] = "I Knew It!",
@@ -24,15 +26,16 @@ Locales['en'] = {
     ['intro_msg_text_final2'] = "I also gave your number to a few friends. Maybe you'll hear from them.",
     ['intro_msg_text_fail'] = "I was unable to log in. Are you even in the correct place?",
     ['intro_msg_text_anon'] = "Anon doesn't work on me, n00b! ",
+    ['intro_msg_text_anon_obv'] = "Hah, you think a blocked number works on me?\n",
     ['intro_file_content'] = "Good work!\nNow let's get to business. I need the hostname of the main machine of Lester Crest. " ..
             "We both know who I'm talking about.\n\nGo to his house, get the information, write it to my number.\nEasy, right?",
-    ['life_invader_marker'] = "LifeInvader",
+
     ['mission_1_msg_text_final'] = "Good work.",
     ['mission_1_msg_text_fail'] = "What? Is that even a valid version number?",
-    ['intro_msg_text_anon_obv'] = "Hah, you think a blocked number works on me?\n",
     ['mission_1_msg_subtitle'] = "LifeInvader",
     ['mission_1_msg_subtitle_re'] = "Re: LifeInvader",
     ['mission_1_msg_text_start'] = "I need the OS version of the LifeInvader admin machine. Hurry.",
+
     ['mission_2_msg_text_final'] = "I'm in, thanks.",
     ['mission_2_msg_text_fail'] = "What? That can't be right. Check again!",
     ['mission_2_msg_subtitle'] = "Re: LifeInvader",
